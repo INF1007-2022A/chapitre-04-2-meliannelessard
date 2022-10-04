6e4160b6131d7b7b6ddf87fced4874346026b7b5
meliannelessard <112814081+meliannelessard@users.noreply.github.com>
2022-10-04 13:05:01 -0400

    v

--- exercice.py
+++ exercice.py
@@ -5,28 +5,34 @@ import random
 
 def get_first_part_of_name(name):
 	partie1 = name.split("-")[0] # liste -> ['Jean', 'Luc']
+	print(partie1)
+	nouveau = partie1[0].upper() + partie1[1:].lower()
 
 	# mettre premiere lettre en majuscule
-	majuscule = partie1[0].upper() + partie1[1:].lower()
 	
-	return "Bonjour, " + majuscule
+	
+	return (f'Bonjour {nouveau}')
 
 def get_random_sentence(animals, adjectives, fruits):
 	
-	animal = animals[random.randrange(0, len(animals))]
-	adjectif = adjectives[random.randrange(0, len(adjectives))]
-	fruit = fruits[random.randrange(0, len(fruits))] 
+	#animal = animals[random.randrange(0, len(animals))]
+	#adjectif = adjectives[random.randrange(0, len(adjectives))]
+	#fruit = fruits[random.randrange(0, len(fruits))] 
 
 	# plus facile de faire une boucle
 
-	return (f"Aujourd'hui j'ai vu un {animal} s'emparer d’un panier {adjectif} plein de {fruit}.»")
+	animals = animals[random.randrange(len(animals))]
+	adjectives = adjectives[random.randrange(len(adjectives))]
+	fruits = fruits[random.randrange(len(fruits))]
+	return (f'Aujourd’hui, j’ai vu un {animals} s’emparer d’un panier {adjectives} plein de {fruits}.')
+	
 
 def encrypt(text, shift):
 
-	return ""
+	return encrypt(text, shift)
 
 def decrypt(encrypted_text, shift):
-	return ""
+	return decrypt(encrypted_text, shift)
 
 
 if __name__ == "__main__":
